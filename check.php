290fc455538cb9e25401aeaac8f79bb8956951d4
丹野光 <curryhar@gmail.com>
2019-08-28 15:39:09 +0800

    送信処理追加

---
+++ check.php
@@ -0,0 +1,28 @@
+<?php
+// スーパーグローバル変数
+// PHPがもともと用意している変
+
+var_dump($_POST);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>入力確認</title>
+</head>
+<body>
+  <h1>入力確認</h1>
+  <p>名前:<?php echo $_POST['username']; ?></p>
+  <p>メールアドレス:<?php echo $_POST['email']; ?></p>
+  <p>お問い合わせ内容:<?php echo $_POST['content']; ?></p>
+  <form action="">
+    <button>戻る</button>
+    <input type="submit" value="OK">
+  </form>
+
+</body>
+</html>
